1fee705640b4f26400fc476e3535b808c7e8d726
EmilOMegalla <emilmegalla3@gmail.com>
2024-06-01 18:40:33 +1000

    Update script.js
    If this applied, it will create five objects inside the pokemonList array.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 console.log('hi');
 
-let pokemonList = [];
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+    { name: 'Vanusaur', height: 2, types: ['grass', 'poison'] },
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']}
+];
 
 
