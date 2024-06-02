35cfba32d662a6f74b5a19f15389ee87ba4ed930
EmilOMegalla <emilmegalla3@gmail.com>
2024-06-02 13:34:53 +1000

    update script.js file
    If this apply. It will add a comment to the code for explaination.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ let pokemonList = [
     {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']}
 ];
 
-// The bellow code block will loop over the array of pok
+// The bellow code block will loop over the array of pokemonList.
+// Then it will check the Bokemon's height if it bigger than 1.7, if yes it will print the sentence "wow that's big".
+// If the condition of hiegh was not met, it will then display the name and the height of the bokemon.
 for (let i = 0; i < pokemonList.length; i++){
      if (pokemonList[i].height > 1.7) {
         document.write('\" ' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') <strong>Wow, that\'s big!\</strong>"<br>');
      } else {
-         document.write('\" ' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')\"<br>');
+         document.write('\" ' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')\"<br>');
     }
    
 }
