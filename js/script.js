68e115d6e04fbf2895412330b4035c1837c493a8
EmilOMegalla <emilmegalla3@gmail.com>
2024-06-02 13:12:49 +1000

    update script
    if this applied, it create pokeList array that has five object inside it)
    if this applied, it create a for loop to loop over the array objects, and print their names and hiegh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log('hi');
-
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
     { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
@@ -8,4 +6,12 @@ let pokemonList = [
     {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']}
 ];
 
-
+// The bellow code block will loop over the array of pok
+for (let i = 0; i < pokemonList.length; i++){
+     if (pokemonList[i].height > 1.7) {
+        document.write('\" ' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') <strong>Wow, that\'s big!\</strong>"<br>');
+     } else {
+         document.write('\" ' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')\"<br>');
+    }
+   
+}
